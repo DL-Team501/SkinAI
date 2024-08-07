aaa66626f42a5934feaff45d707d65c5fb86d991
oriyafe1 <ori987yafe@gmail.com>
2024-08-07 15:39:19 +0300

    Add all_products_data.json

--- data/scrape_sephora/fetch_products_data.py
+++ data/scrape_sephora/fetch_products_data.py
@@ -1,4 +1,5 @@
 import json
+import math
 import time
 from json import JSONDecodeError
 
@@ -8,37 +9,37 @@ import requests
 with open('products.json', 'r') as json_file:
     products_metadata = json.load(json_file)
 
-counter = 0
+start_index = 2700
 
 
-def get_product_data(product_metadata):
-    global counter
+def get_product_data(curr_index, product_metadata):
 
     response = requests.get('https://sephora.p.rapidapi.com/us/products/v2/detail',
                             headers={"x-rapidapi-host": "sephora.p.rapidapi.com",
-                                     "x-rapidapi-key": "ead2eb722amsh72549df50a19bc5p16bd69jsnad8ebf09cce1"},
+                                     "x-rapidapi-key": "8ec274ab7cmsh7bd26823d4a26d7p13e71ajsn0ee25cf04fa2"},
                             params={'productId': product_metadata['id'], 'preferedSku': product_metadata['skuId']})
 
     try:
-        print(counter)
         print(response.text)
+        print(curr_index)
         response_data = response.json()
         product_data = {'displayName': pydash.get(response_data, 'productDetails.displayName'),
                         'longDescription': pydash.get(response_data, 'productDetails.longDescription'),
                         'shortDescription': pydash.get(response_data, 'productDetails.shortDescription'),
                         'suggestedUsage': pydash.get(response_data, 'productDetails.suggestedUsage'),
                         'ingredientDesc': pydash.get(response_data, 'currentSku.ingredientDesc')}
-        counter += 1
 
         return pydash.merge(product_data, product_metadata)
     except JSONDecodeError:
         print(response.text)
         time.sleep(1)
 
-        return get_product_data(product_metadata)
+        return get_product_data(curr_index, product_metadata)
 
 
-products_data = list(map(lambda product_metadata: get_product_data(product_metadata), products_metadata))
+products_data = list(
+    map(lambda indexed_product_metadata: get_product_data(indexed_product_metadata[0], indexed_product_metadata[1]),
+        enumerate(products_metadata[start_index:min(start_index + 450, len(products_metadata))])))
 
-with open('products_data.json', 'w') as json_file:
+with open('products_data-7.json', 'w') as json_file:
     json.dump(products_data, json_file, indent=4)
