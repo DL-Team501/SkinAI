8f6c60510f02b1c427964b1912b9d819b9248787
fannyabr <fannyabramovich@gmail.com>
2024-08-14 16:11:27 +0300

    model args clearer names

--- src/models/ingredients_classification_model.py
+++ src/models/ingredients_classification_model.py
@@ -5,12 +5,12 @@ from src.models.classification_head import ClassificationHead
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, embed_size, max_len=512):
+    def __init__(self, embed_size, tokenized_vector_len):
         super(PositionalEncoding, self).__init__()
-        self.encoding = torch.zeros(max_len, embed_size)
+        self.encoding = torch.zeros(tokenized_vector_len, embed_size)
         self.encoding.requires_grad = False
 
-        pos = torch.arange(0, max_len).unsqueeze(1).float()
+        pos = torch.arange(0, tokenized_vector_len).unsqueeze(1).float()
         _2i = torch.arange(0, embed_size, step=2).float()
 
         self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / embed_size)))
@@ -22,20 +22,22 @@ class PositionalEncoding(nn.Module):
 
 
 class SkincareClassifier(nn.Module):
-    def __init__(self, vocab_size, embed_size, num_labels, max_len=216):
+    def __init__(self, vocab_size, num_labels, max_len, embed_size, nhead, num_layers):
         super(SkincareClassifier, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.positional_encoding = PositionalEncoding(embed_size, max_len)
         self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=embed_size, nhead=8),
-            num_layers=3
+            nn.TransformerEncoderLayer(d_model=embed_size, nhead=nhead),
+            num_layers=num_layers
         )
         self.classifier = ClassificationHead(embed_size, num_labels, hidden_sizes=[embed_size // 2], dropout=0.2)
 
     def forward(self, input_ids):
         embedded = self.embedding(input_ids)
         embedded = self.positional_encoding(embedded)
+
         transformer_output = self.transformer(embedded)
         pooled_output = transformer_output.mean(dim=1)  # Pooling
         logits = self.classifier(pooled_output)
+
         return logits
--- src/training.py
+++ src/training.py
@@ -20,7 +20,7 @@ def train_model(model, train_loader, val_loader, epochs, learning_rate, experime
     mlflow.set_experiment(experiment_name)  # Create or use existing experiment
 
     with mlflow.start_run() as run:
-        # mlflow.log_params(model_args)
+        mlflow.log_params(model_args)
 
         for epoch in range(epochs):
             print(f"Epoch number : {epoch}")
@@ -86,22 +86,17 @@ if __name__ == "__main__":
     train_dataloader, val_dataloader = create_dataloaders()
 
     vocab_size = 5868
-    embed_size = 128
+    ingredients_vector_len = 216
     num_labels = 5  # Number of skin types
 
     # Transformer encoder parameters
-    # model_args = {
-    #     'd_model': 768,  # Embedding dimension
-    #     'nhead': 2,  # Number of heads in multi-head attention
-    #     'num_transformer_layers': 6,  # Number of transformer encoder layers
-    #     'dim_feedforward': 1024,  # Dimension of feedforward network in the transformer
-    #     'dropout': 0.1,  # Dropout probability
-    # }
-    #
-    # ingredients_vector_len = 216
+    model_args = {
+        'embed_size': 128,  # Embedding dimension
+        'nhead': 2,  # Number of heads in multi-head attention
+        'num_layers': 6,  # Number of transformer encoder layers
+    }
 
-    model = SkincareClassifier(vocab_size, embed_size, num_labels)
-    # model = CosmeticEfficacyModel(ingredients_vector_len, 5, [768 // 2, (768 // 2) // 2], **model_args)
+    model = SkincareClassifier(vocab_size, num_labels, ingredients_vector_len, **model_args)
 
     train_model(model, train_dataloader, val_dataloader, epochs=50, learning_rate=0.001,
                 experiment_name=experiment_name)
