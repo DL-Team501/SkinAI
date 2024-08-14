13d0625c94fd2267f898401b1605dfd26d10d322
fanny <fannyabramovich@gmail.com>
2024-08-14 18:37:00 +0300

    update model classes

--- src/consts.py
+++ src/consts.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
 ROOT_PATH = str(Path(__file__).parent.parent)
+
+INGREDIENT_LIST_CLASSIFICATION_LABELS = ['combination', 'dry', 'normal', 'oily', 'sensitive',
+                                         'pores', 'oiliness', 'dryness', 'dullness', 'uneven',
+                                         'wrinkles', 'acne', 'blemishes', 'redness', 'dark', 'puffiness']
